078cd6cfe72c4d12a3c250d727c97d5ff1e11200
RichardSyfer <deadwalker87@gmail.com>
2018-05-25 00:22:13 +0300

    add #check_code

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -5,7 +5,24 @@ module Codebreaker
     end
 
     def start
-      @secret_code = Array.new(4).map { rand(1..7) }
+      @secret_code = Array.new(4) { rand(1..6) }
+    end
+
+    def code_check(code)
+      if code.match(/^[1-6]{4}$/)
+        code.split('').map.with_index { |v, i|
+          if @secret_code.include?(v.to_i)
+            @secret_code[i] == v.to_i ? '+' : '-'
+          end
+        }.compact.sort.join(' ')
+      else
+        warn 'Incorrect input'
+      end
     end
   end
 end
+
+# game =Codebreaker::Game.new
+# p sc = game.start
+# p code = '1234'
+# p game.code_check(code)
--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -6,7 +6,7 @@ module Codebreaker
       before(:all) do
         @game = Game.new
         @game.start
-        @secret_code = @game.instance_variable_get(:@secret_code)
+        @secret_code = @game.instance_variable_get(:@secret_code).join
       end
       it 'generates and saves secret code with numbers from 1 to 6' do
         expect(@secret_code).to match(/^[1-6]{4}$/)
