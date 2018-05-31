bfee7245092aae472668477616e2e8fd9e9c572f
RichardSyfer <deadwalker87@gmail.com>
2018-05-31 06:43:14 +0300

    nearly ready codebreaker, need examine #check_code

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,28 +1,69 @@
+require 'date'
+require 'yaml'
+
 module Codebreaker
   class Game
+    HINTS = 3
+    ATTEMPTS = 10
+
     def initialize
-      @secret_code = ''
+      @secret_code = []
+      @breaker_code = []
+      @hints_count = HINTS
+      @attempts_remain = ATTEMPTS
     end
 
     def start
       @secret_code = Array.new(4) { rand(1..6) }
     end
 
+    def win?
+      @breaker_code == @secret_code
+    end
+
+    def lose?
+      @attempts_remain == 0
+    end
+
+    def game_over?
+      win? || lose?
+    end
+
+    def hints
+      return p 'No hints remains' if @hints_count.zero?
+      hint = @secret_code - @breaker_code
+      return p 'You guessed all numbers, try swap them' if hint.empty?
+      p "HINT: #{hint.sample}, #{@hints_count -= 1} hints remains"
+    end
+
+    def secret
+      @secret_code.join
+    end
+
+    # TODO: #check
     def code_check(code)
-      if code.match(/^[1-6]{4}$/)
-        code.split('').map.with_index { |v, i|
-          if @secret_code.include?(v.to_i)
-            @secret_code[i] == v.to_i ? '+' : '-'
-          end
-        }.compact.sort.join(' ')
-      else
-        warn 'Incorrect input'
-      end
+      @breaker_code = code.split('').map(&:to_i)
+
+      result = @breaker_code.map.with_index { |v, i|
+        if @secret_code.include?(v)
+          @secret_code[i] == v ? '+' : '-'
+        end
+      }.compact.sort.join(' ')
+
+      p "Attempt result: #{result}"
+      p "Remaining attempts: #{@attempts_remain -= 1}"
+    end
+
+    def save_result
+      p 'Input your name: '
+      codebreaker_name = gets.chomp
+      game_data = {
+        game_date: Date.today.to_s,
+        player_name: codebreaker_name,
+        attempts: "#{ATTEMPTS - @attempts_remain}",
+        result: win? ? 'won' : 'lost'
+      }
+      File.open('game_data.yml', 'w') { |f| f.write YAML.dump(game_data) }
     end
   end
 end
-
-# game =Codebreaker::Game.new
-# p sc = game.start
-# p code = '1234'
-# p game.code_check(code)
---
+++ lib/codebreaker/hmi.rb
@@ -0,0 +1,49 @@
+module Codebreaker
+  class Hmi
+    def initialize
+      @game = Game.new
+    end
+
+    def launch_game
+      greeting
+      @game.start
+      until @game.game_over?
+        answ = gets.chomp
+        if answ =~ /^[1-6]{4}$/
+          @game.code_check(answ)
+        elsif answ =~ /^[Hh]{1}$/
+          @game.hints
+        elsif answ =~ /^[Qq]{1}$/
+          return
+        else
+          p 'Incorrect input'
+        end
+      end
+      p @game.win? ? 'Superb game! You won' : "Sorry, but you, code was #{@game.secret}"
+      save_offer
+      replay_offer
+    end
+
+    private
+
+    def greeting
+      p 'Welcome to Codebreaker!'
+      p 'Lets try break the code'
+      p 'Enter suggested code OR "h" for hint, "q" for exit'
+    end
+
+    def save_offer
+      p 'Would you like save result'
+      @game.save_result if gets.chomp =~ /^[Yy]{1}$/
+    end
+
+    def replay_offer
+      p 'Would you like play again?'
+      if gets.chomp =~ /^[Yy]{1}$/
+        @game = Game.new
+        system('clear')
+        launch_game
+      end
+    end
+  end
+end
